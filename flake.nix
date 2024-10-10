d6b47c3b598cfbb9db1c53e34faab1fa29c512a7
Darwin D Wu <darwin67@users.noreply.github.com>
2024-10-10 10:03:46 -0700

    add nix configuration for local development

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Tanshuku - URL shortner";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            elixir
+            erlang_27
+            nodejs_18 # need this to install inngest-cli for now
+
+            # LSPs
+            # elixir-ls
+            lexical
+            erlang-ls
+
+            # Tools
+            yamllint
+            shfmt
+            shellcheck
+            git-cliff
+          ];
+        };
+      });
+}
